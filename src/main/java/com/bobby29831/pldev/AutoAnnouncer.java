35ad52dd8eff800d659c53efb719d10c8a504a5c
bobby29831 <bobby29831@gmail.com>
2021-02-21 13:40:09 -0600

    Initial commit

---
+++ src/main/java/com/bobby29831/pldev/AutoAnnouncer.java
@@ -0,0 +1,17 @@
+package com.bobby29831.pldev;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AutoAnnouncer extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
